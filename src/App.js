0726c3f60c308ea6b2527d46a088939cc37a67ad
Jorge Luis Arteaga <64978772+jlarteagap@users.noreply.github.com>
2020-09-04 12:48:29 -0400

    Firts commit
    Firts commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import BboysList from './components/BboysList'
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Switch>
+          <Route path="/">
+            <BboysList />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
-
-export default App;
---
+++ src/components/BboysList.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import axios from 'axios';
+
+export default class BboysList extends React.Component {
+  state = {
+    bboys: []
+  }
+
+  componentDidMount() {
+    axios.get(`http://localhost:8000/api/v1/bboys/`)
+      .then(res => {
+        const bboys = res.data;
+        this.setState({ bboys });
+      })
+  }
+
+  render() {
+    return (
+      <ul>
+        { this.state.bboys.map(bboy => <li>{bboy.name}</li>)}
+      </ul>
+    )
+  }
+}
