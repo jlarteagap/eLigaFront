33fdd8314685e9832a600e23d4901461bb8b95ed
Jorge Arteaga <jorgelarteagap@gmail.com>
2020-09-29 19:00:45 -0400

    Updates

---
+++ precache-manifest.01be0f91bba76c83b723627be8d0ed54.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5faa608a0d7ff772f0f933ae30acca8e",
+    "url": "/eLigaFront/index.html"
+  },
+  {
+    "revision": "736ca60c0f5602ac5410",
+    "url": "/eLigaFront/static/css/main.fed4e68c.chunk.css"
+  },
+  {
+    "revision": "598523c0ddb7d3323593",
+    "url": "/eLigaFront/static/js/2.7a130def.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/eLigaFront/static/js/2.7a130def.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "736ca60c0f5602ac5410",
+    "url": "/eLigaFront/static/js/main.517241dd.chunk.js"
+  },
+  {
+    "revision": "16698729cc2eb9de2ca1",
+    "url": "/eLigaFront/static/js/runtime-main.c3e0bbcd.js"
+  },
+  {
+    "revision": "7138c8c5dc497a80cb922d215c030672",
+    "url": "/eLigaFront/static/media/slide.7138c8c5.jpg"
+  }
+]);
